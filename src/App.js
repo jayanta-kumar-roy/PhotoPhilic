a1e288c290471110cf5b001278b9261b4cdc8fd0
jayanta-kumar-roy <35413962+jayanta-kumar-roy@users.noreply.github.com>
2021-07-04 15:54:02 +0530

    Props added in Post components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React,{useState} from 'react';
 import './App.css';
 import Post from './Post'
 function App() {
+const [posts,setPosts]=useState([]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -12,7 +15,7 @@ function App() {
       <h1>Lets Build our photophilic app</h1>
       <Post username="Jayanta" caption="this is so cool" imageUrl="https://images.unsplash.com/photo-1622495546323-5dac33dedb01?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60" />
       <Post username="Touqeer" caption="I'm loving this" imageUrl="https://images.unsplash.com/photo-1625231066641-7ab5b9c234d3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>
-      <Post username="Kundan" caption="This is beautiful" imageUrl="https://images.unsplash.com/photo-1625325544162-3951bfdf8831?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>
+      <Post username="Kundan" caption="This is beautiful " imageUrl="https://images.unsplash.com/photo-1625325544162-3951bfdf8831?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>
       
       </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Post.css'
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username,caption,imageUrl}) {
     return (
       <div className="post">
         <div className="post__header">
@@ -11,19 +11,19 @@ function Post() {
             alt="JayantaRoy"
             src="/static/images/aatar/1.jpg"
           ></Avatar>
-          <h3>Username</h3>
+          <h3>{username}</h3>
         </div>
 
         {/*header->avatar + username*/}
 
         <img
           className="post__image"
-          src="https://images.unsplash.com/photo-1527856263669-12c3a0af2aa6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMHBpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          src={imageUrl}
         ></img>
         {/*image */}
 
         <h4 className="post__text">
-          <strong>Username</strong>:caption
+          <strong>{username}</strong>:{caption}
         </h4>
         {/*username + caption */}
       </div>
