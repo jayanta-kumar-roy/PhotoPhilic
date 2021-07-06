5ddb55248a98909064529f24f85a622954dba4ca
jayanta-kumar-roy <35413962+jayanta-kumar-roy@users.noreply.github.com>
2021-07-06 15:56:24 +0530

    modal added for signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import Post from './Post'
-import {db} from './firebase'
+import Post from './Post';
+import {db} from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button,Input } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50 ;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
+const classes = useStyles();
+const [modalStyle] = useState(getModalStyle);
+
+
 const [posts,setPosts]=useState([]);
+const [open,setOpen] = useState(false); 
+const [username,setUsername] = useState(''); 
+const [password,setPassword] = useState(''); 
+const [email,setEmail] = useState(''); 
 
   useEffect(()=>{
     db.collection('posts').onSnapshot(snapshot=>{
@@ -16,24 +50,65 @@ const [posts,setPosts]=useState([]);
     })
   },[]);
 
+  const signUp =(event)=>{
+
+  }
+  
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            ></Input>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Input>
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Input>
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         ></img>
       </div>
+      <Button onClick={() => setOpen(true)}>Sign up</Button>
+
       <h1>Lets Build our photophilic app</h1>
-      
-      {
-        posts.map(({id,post})=>(
-          <Post  key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
-        ))
-      } 
-      
-      
-      </div>
+
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
+    </div>
   );
 }
 
